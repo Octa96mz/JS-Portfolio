88faa339f5d709ba7209f06a851dc022e2445eff
Octa96mz <octa96mz@gmail.com>
2023-04-28 20:43:42 -0300

    correccion de info

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -27,13 +27,13 @@ const Template = async () => {
           </ul>
         </div>
         <div class="card_social">
-          <a href="https://twitter.com/gndx">
+          <a href="https://twitter.com/octamontoya">
           <img src="${twitter}" />
           </a>
-          <a href="https://github.com/gndx">
+          <a href="https://github.com/Octa96mz">
             <img src="${github}" />
           </a>
-          <a href="https://instagram.com/gndx">
+          <a href="https://instagram.com/octamontoya">
             <img src="${instagram}" />
           </a>
         </div>
